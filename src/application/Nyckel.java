764b5cea0375714f3370a0a5afc8dff168553d84
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-03-03 14:12:02 +0100

    cnasbges

--- src/application/Nyckel.java
+++ src/application/Nyckel.java
@@ -1,5 +1,13 @@
 package application;
 
 public class Nyckel {
-
+	Anställd anställd; //behövs dubbelkollpling?
+	private String nyckelID;
+	
+	public String getNyckelID() {
+		return nyckelID;
+	}
+	public void setNyckelID(String nyckelID) {
+		this.nyckelID = nyckelID;
+	}
 }
--- src/application/Sal.java
+++ src/application/Sal.java
@@ -1,5 +1,5 @@
 package application;
 
 public class Sal {
-
+	Nyckel nyckel;
 }
